c7ea070e8a3a55364af1ae931c6504e15bc1d56a
rStar <starlove124760@gmail.com>
2021-05-14 23:45:17 +0800

    feat: add Test - ActorComponentTests

---
+++ Assets/Game/Scripts/Main/ViewComponent/ActorComponent.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Main.ViewComponent
+{
+    public class ActorComponent : MonoBehaviour
+    {
+    #region Public Variables
+
+        public SpriteRenderer spriteRenderer;
+        public Text           text_IdAndDataId;
+
+    #endregion
+
+    #region Public Methods
+
+        public void SetSprite(Sprite sprite)
+        {
+            spriteRenderer.sprite = sprite;
+        }
+
+
+        public void SetText(string displayText)
+        {
+            text_IdAndDataId.text = displayText;
+        }
+
+    #endregion
+    }
+}
