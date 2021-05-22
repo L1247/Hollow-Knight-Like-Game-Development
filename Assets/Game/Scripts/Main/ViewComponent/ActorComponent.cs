cf07c6fd36f702814864c85a533f1eb7326fe7fe
rStar <starlove124760@gmail.com>
2021-05-22 22:16:15 +0800

    create Unity component and binding it

--- Assets/Game/Scripts/Main/ViewComponent/ActorComponent.cs
+++ Assets/Game/Scripts/Main/ViewComponent/ActorComponent.cs
@@ -87,9 +87,10 @@ namespace Main.ViewComponent
 
         private void Awake()
         {
-            _transform = transform;
-            rigi2d     = GetComponent<Rigidbody2D>();
-            isOnGround = true;
+            _transform     = transform;
+            rigi2d         = GetComponent<Rigidbody2D>();
+            isOnGround     = true;
+            UnityComponent = new UnityComponent(animator);
         }
 
         private void MoveCharacter()
@@ -114,6 +115,33 @@ namespace Main.ViewComponent
     #region Public Methods
 
         void PlayAnimation(string animationName);
+
+    #endregion
+    }
+
+    public class UnityComponent : IUnityComponent
+    {
+    #region Private Variables
+
+        private readonly Animator animator;
+
+    #endregion
+
+    #region Constructor
+
+        public UnityComponent(Animator animator)
+        {
+            this.animator = animator;
+        }
+
+    #endregion
+
+    #region Public Methods
+
+        public void PlayAnimation(string animationName)
+        {
+            animator?.Play(animationName);
+        }
 
     #endregion
     }
