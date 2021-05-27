d12cc63e6d6070fa2834c9c02e35571b5cea106b
rStar <starlove124760@gmail.com>
2021-05-27 23:50:46 +0800

    extract method - Should_CanMoving_Response

--- Assets/Game/Scripts/MainTests/ActorTests/CharacterConditionTests.cs
+++ Assets/Game/Scripts/MainTests/ActorTests/CharacterConditionTests.cs
@@ -1,26 +1,52 @@
+#region
+
 using Main.ViewComponent;
 using NUnit.Framework;
+
+#endregion
 
 namespace MainTests.ActorTests
 {
     public class CharacterConditionTests
     {
+    #region Private Variables
+
+        private CharacterCondition characterCondition;
+
+    #endregion
+
+    #region Setup/Teardown Methods
+
+        [SetUp]
+        public void SetUp()
+        {
+            characterCondition = new CharacterCondition();
+        }
+
+    #endregion
+
     #region Test Methods
 
         [Test]
         public void Should_Return_True_When_Get_CanMoving()
         {
-            var characterCondition = new CharacterCondition();
-            var canMoving          = characterCondition.CanMoving();
-            Assert.AreEqual(true , canMoving);
+            Should_CanMoving_Response(true);
         }
 
         [Test]
         public void Should_Return_False_When_IsMoving_False()
         {
-            var characterCondition = new CharacterCondition();
-            var canMoving          = characterCondition.CanMoving();
-            Assert.AreEqual(false , canMoving);
+            Should_CanMoving_Response(false);
+        }
+
+    #endregion
+
+    #region Private Methods
+
+        private void Should_CanMoving_Response(bool expectedCanMoving)
+        {
+            var canMoving = characterCondition.CanMoving();
+            Assert.AreEqual(expectedCanMoving , canMoving);
         }
 
     #endregion
